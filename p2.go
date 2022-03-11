1909c333628469bb1e74ef252cdb762ceec8ca15
Isaiah Stapleton <isaiahstapleton@gmail.com>
2022-03-11 17:03:26 -0500

    Added wait groups in order to implement process synchronization & added readme

--- p2.go
+++ p2.go
@@ -1,19 +1,33 @@
-package p2
+package main
 
 import (
 	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 // Consumer task to operate on queue
 func consumer_task(task_num int) {
-	fmt.Printf("I'm consumer task #%v", task_num)
+	
+	fmt.Printf("I'm consumer task #%v ", task_num)
+	fmt.Println("Line being popped off queue: " + queue[0])
+
+	queue = queue[1:]
+
+
 }
+
+// Initialize queue
+var queue = make([]string, 0)
 
 
 func main() {
+
+	// Initialize wait group 
+	var wg sync.WaitGroup
+
 
 	// Get number of tasks to run from user
 	var numof_tasks int
@@ -35,8 +49,6 @@ func main() {
         log.Fatal(err)
     }
 
-	// Initialize queue
-	queue := make([]string, 0)
 
 	// Loop through each line in the file and append it to the queue
 	for scanner.Scan() {
@@ -46,7 +58,15 @@ func main() {
 
 	// Start specified # of consumer tasks
 	for i := 1; i <= numof_tasks; i++ {
-		go consumer_task(i)
+		wg.Add(1)
+		go func(i int) { 
+			consumer_task(i) 
+			wg.Done()
+		}(i)
 	}
 
+
+	wg.Wait()
+	fmt.Println("All done")
+	fmt.Println(queue)
 }
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Isaiah Stapleton & Emma Steuer
+
+# This purpose of this project is to build understanding of threading and process synchronization
+
+# The purpose of this program is to implement a single producer/multiple consumer queue
