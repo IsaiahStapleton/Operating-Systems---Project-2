daf0c6647cb6c7437db4e59ea78434f16bb567b4
Isaiah Stapleton <isaiahstapleton@gmail.com>
2022-03-23 18:21:54 -0400

    Final touch ups

--- p2.go
+++ p2.go
@@ -5,29 +5,22 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"regexp"
+	"strings"
 	"sync"
 )
 
-// Function to count the number of words in a string
-func count_words(value string) int {
-    // Match non-space character sequences.
-    re := regexp.MustCompile(`[\S]+`)
-    
-    // Find all matches and return count.
-    results := re.FindAllString(value, -1)
-    return len(results)
+// Counts the number of words in a string
+func count_words(s string) int {
+	return len(strings.Fields(s))
 }
 
 // Consumer task to operate on queue
 func consumer_task(task_num int, ch <-chan string, ch2 chan<- int) {
 
-
-
 	// Loop through the channel and output desired information and send word count of line back through the channel
 	// in order to communicate with other threads
 	for item := range ch {
-		word_count := count_words(item)
+		word_count := count_words(item) 
 		fmt.Printf("\nTask %d consuming line: %v\nNumber of words: %d", task_num, item, word_count)
 		ch2 <- word_count
 	}
@@ -38,8 +31,8 @@ func main() {
 
 	// Initialize queue
 	queue := make(chan string)
-	total_words := make(chan int)
-	var totalWords = 0
+	word_counts := make(chan int)
+	total_words := 0
 
 	// Initialize wait group
 	var wg sync.WaitGroup
@@ -51,6 +44,7 @@ func main() {
 	fmt.Scanf("%d %s", &numof_tasks, &file_name)
 
 
+	// Read the specified file in
 	file, err := os.Open(file_name)
 	if err != nil {
 		log.Fatal(err)
@@ -63,36 +57,37 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Start consumer tasks and add them to the wait group
 	for i := 1; i <= numof_tasks; i++ {
 		wg.Add(1)
 		go func(i int) {
-			consumer_task(i, queue, total_words)
+			consumer_task(i, queue, word_counts)
 			wg.Done()
 		}(i)
 	}
 
-	// Loop through each line in the file and append it to the queue
+	// Loop through each line in the file and append it to the queue, when all lines have been read
+	// signal the queue channel to close
 	go func() {
-		// Loop through each line in the file and append it to the queue
+	
 		for scanner.Scan() {
-			queue <- scanner.Text()
-			
+			queue <- scanner.Text()		
 		}
-		// Signals consumer threads that there is no more lines to be read
 		close(queue) 
 	}()
 
 
+	// Accumlate the word counts, when all word counts have been accumulated signal the word_counts channel to close
 	go func() {
-		for wordCount := range total_words {
-			totalWords += wordCount
+		for words := range word_counts {
+			total_words += words
 		}
-		close(total_words)
+		close(word_counts)
 	}()
 
+	// Wait for threads to finish executing and then output the total word count
 	wg.Wait()
-
 	fmt.Printf("\nDone")
+	fmt.Printf("\nTotal number of words: %d\n", total_words)
 
-	fmt.Printf("\nTotal number of words: %d\n", totalWords)
 }
