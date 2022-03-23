164ad1964557181de51e6873cf39033ff544d862
Isaiah Stapleton <isaiahstapleton@gmail.com>
2022-03-23 17:25:42 -0400

    Implemented communication between channels to make it synchronous

--- p2.go
+++ p2.go
@@ -5,49 +5,56 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
+	"regexp"
 	"sync"
 )
 
-var total_num_word int
-
-func count_words(s string) int {
-	return len(strings.Fields(s))
+// Function to count the number of words in a string
+func count_words(value string) int {
+    // Match non-space character sequences.
+    re := regexp.MustCompile(`[\S]+`)
+    
+    // Find all matches and return count.
+    results := re.FindAllString(value, -1)
+    return len(results)
 }
 
 // Consumer task to operate on queue
-func consumer_task(task_num int, ch <-chan string) {
+func consumer_task(task_num int, ch <-chan string, ch2 chan<- int) {
 
-	
+
+
+	// Loop through the channel and output desired information and send word count of line back through the channel
+	// in order to communicate with other threads
 	for item := range ch {
-		fmt.Printf("Task %d consuming line: %v\n", task_num, item)
 		word_count := count_words(item)
-		fmt.Printf("	Number of words in line: %d\n", word_count)
-		total_num_word += word_count
+		fmt.Printf("\nTask %d consuming line: %v\nNumber of words: %d", task_num, item, word_count)
+		ch2 <- word_count
 	}
-	// each thread will drop out of their loop when channel is closed
+
 }
 
 func main() {
+
 	// Initialize queue
 	queue := make(chan string)
+	total_words := make(chan int)
+	var totalWords = 0
+
 	// Initialize wait group
 	var wg sync.WaitGroup
-	// Get number of tasks to run from user
-	// var numof_tasks string
-	fmt.Printf("Enter number of tasks to run: ")
-	// stdin starting to get input from user
-	reader := bufio.NewReader(os.Stdin)
-	numof_tasks, _, err := reader.ReadRune()
-	// change rune value to int value
-	numof_tasksi := int(numof_tasks)
 
-	// Open file
-	file, err := os.Open("test.txt")
+	// Get num of tasks to run as well as name of the file from user
+	fmt.Printf("Enter number of tasks to run followed by name of text file (ex. 5 text.txt): ")
+	var numof_tasks int = 0
+	var file_name string
+	fmt.Scanf("%d %s", &numof_tasks, &file_name)
+
+
+	file, err := os.Open(file_name)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	defer file.Close()
 
 	// Scanner to scan the file
@@ -56,26 +63,36 @@ func main() {
 		log.Fatal(err)
 	}
 
+	for i := 1; i <= numof_tasks; i++ {
+		wg.Add(1)
+		go func(i int) {
+			consumer_task(i, queue, total_words)
+			wg.Done()
+		}(i)
+	}
+
 	// Loop through each line in the file and append it to the queue
 	go func() {
 		// Loop through each line in the file and append it to the queue
 		for scanner.Scan() {
 			queue <- scanner.Text()
+			
 		}
-		close(queue) // signal to workers that there is no more items
+		// Signals consumer threads that there is no more lines to be read
+		close(queue) 
 	}()
 
-	// Start specified # of consumer tasks
-	for i := 1; i <= numof_tasksi; i++ {
-		wg.Add(1)
-		go func(i int) {
-			consumer_task(i, queue) 
-			wg.Done()
-		}(i)
-	}
+
+	go func() {
+		for wordCount := range total_words {
+			totalWords += wordCount
+		}
+		close(total_words)
+	}()
 
 	wg.Wait()
-	fmt.Println("All done")
-	fmt.Println(total_num_word)
 
+	fmt.Printf("\nDone")
+
+	fmt.Printf("\nTotal number of words: %d\n", totalWords)
 }
